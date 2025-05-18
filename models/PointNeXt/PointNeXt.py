bb0f627922328321a5fcfcc833b2814535d8d7be
jGrzyb <grzybjaku@gmail.com>
2025-05-18 13:25:51 +0200

    removed print statements

--- models/PointNeXt/PointNeXt.py
+++ models/PointNeXt/PointNeXt.py
@@ -75,72 +75,71 @@ class PointNeXt(nn.Module):
         self.conv = nn.Conv1d(fp1_last, part_classes, 1)
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, None, None]:
-        print(f"Input shape: {x.shape}")
+        # print(f"Input shape: {x.shape}")
 
         coords_s = x[:, :3, :]
-        features_s = x[:, 3:, :]
-        print(f"0 shape: {coords_s.shape} {features_s.shape}")
+        # print(f"0 shape: {coords_s.shape} {features_s.shape}")
 
-        features_0 = self.mlp(x)
-        print(f"features_s shape after mlp: {features_0.shape}")
+        features_0 = self.mlp(x) # should I put features and coords or only features? Currently both
+        # print(f"features_s shape after mlp: {features_0.shape}")
 
         coords_0 = coords_s.permute(0, 2, 1)
         features_0 = features_0.permute(0, 2, 1)
-        print(f"0 shape after permute: {coords_0.shape} {features_0.shape}")
+        # print(f"0 shape after permute: {coords_0.shape} {features_0.shape}")
 
 
         coords_1, features_1 = self.sa1(coords_0, features_0)
-        print(f"1 shape: {coords_1.shape} {features_1.shape}")
+        # print(f"1 shape: {coords_1.shape} {features_1.shape}")
         coords_1, features_1 = self.irmlp1(coords_1, coords_1, features_1)
-        print(f"1 shape after irmlp1: {coords_1.shape} {features_1.shape}")
+        # print(f"1 shape after irmlp1: {coords_1.shape} {features_1.shape}")
 
         coords_2, features_2 = self.sa2(coords_1, features_1)
-        print(f"1 shape: {coords_2.shape} {features_2.shape}")
+        # print(f"1 shape: {coords_2.shape} {features_2.shape}")
         coords_2, features_2 = self.irmlp2(coords_2, coords_2, features_2)
-        print(f"1 shape after irmlp2: {coords_2.shape} {features_2.shape}")
+        # print(f"1 shape after irmlp2: {coords_2.shape} {features_2.shape}")
         coords_2, features_2 = self.irmlp2_1(coords_2, coords_2, features_2)
-        print(f"1 shape after irmlp2_1: {coords_2.shape} {features_2.shape}")
+        # print(f"1 shape after irmlp2_1: {coords_2.shape} {features_2.shape}")
 
         coords_3, features_3 = self.sa3(coords_2, features_2)
-        print(f"1 shape: {coords_3.shape} {features_3.shape}")
+        # print(f"1 shape: {coords_3.shape} {features_3.shape}")
         coords_3, features_3 = self.irmlp3(coords_3, coords_3, features_3)
-        print(f"1 shape after irmlp3: {coords_3.shape} {features_3.shape}")
+        # print(f"1 shape after irmlp3: {coords_3.shape} {features_3.shape}")
 
         coords_4, features_4 = self.sa4(coords_3, features_3)
-        print(f"1 shape: {coords_4.shape} {features_4.shape}")
+        # print(f"1 shape: {coords_4.shape} {features_4.shape}")
         coords_4, features_4 = self.irmlp4(coords_4, coords_4, features_4)
-        print(f"1 shape after irmlp4: {coords_4.shape} {features_4.shape}")
+        # print(f"1 shape after irmlp4: {coords_4.shape} {features_4.shape}")
 
 
-        print(f'before fp4: {coords_3.shape} {coords_4.shape} {features_3.shape} {features_4.shape}')
+        # print(f'before fp4: {coords_3.shape} {coords_4.shape} {features_3.shape} {features_4.shape}')
         features_3 = self.fp4(coords_3, coords_4, features_3, features_4)
-        print(f"features_3 shape after fp4: {features_3.shape}")
+        # print(f"features_3 shape after fp4: {features_3.shape}")
 
-        print(f'before fp3: {coords_2.shape} {coords_3.shape} {features_2.shape} {features_3.shape}')
+        # print(f'before fp3: {coords_2.shape} {coords_3.shape} {features_2.shape} {features_3.shape}')
         features_2 = self.fp3(coords_2, coords_3, features_2, features_3)
-        print(f"features_2 shape after fp3: {features_2.shape}")
+        # print(f"features_2 shape after fp3: {features_2.shape}")
 
-        print(f'before fp2: {coords_1.shape} {coords_2.shape} {features_1.shape} {features_2.shape}')
+        # print(f'before fp2: {coords_1.shape} {coords_2.shape} {features_1.shape} {features_2.shape}')
         features_1 = self.fp2(coords_1, coords_2, features_1, features_2)
-        print(f"features_1 shape after fp2: {features_1.shape}")
+        # print(f"features_1 shape after fp2: {features_1.shape}")
 
-        print(f'before fp1: {coords_0.shape} {coords_1.shape} {features_0.shape} {features_1.shape}')
+        # print(f'before fp1: {coords_0.shape} {coords_1.shape} {features_0.shape} {features_1.shape}')
         features_0 = self.fp1(coords_0, coords_1, features_0, features_1)
-        print(f"features_0 shape after fp1: {features_0.shape}")
+        # print(f"features_0 shape after fp1: {features_0.shape}")
 
 
         x = self.drop(features_0)
         x = x.permute(0, 2, 1)
-        print(f"X shape before conv: {x.shape}")
+        # print(f"X shape before conv: {x.shape}")
 
         x = self.conv(x)
-        print(f"X shape after conv: {x.shape}")
+        # print(f"X shape after conv: {x.shape}")
 
         x = x.permute(0, 2, 1)
-        print(f"Shape after final permute: {x.shape}")
+        # print(f"Shape after final permute: {x.shape}")
 
         x = F.log_softmax(x, dim=-1)
-        print(f"Shape after log_softmax: {x.shape}")
+        # print(f"Shape after log_softmax: {x.shape}")
 
         return x, None, None
 
